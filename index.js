b1f9ae0687716e853b79af7196982084f29a0998
Buddha Lo <lo_977@yahoo.com>
2024-02-09 10:30:43 -0800

    contenetLoaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Your code goes 
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("text").textContent = "This is really cool!"
+});
+
+//   function updateDOM(){
+//     document.getElementById("text").innerHTML="This is really cool!";
+//   }
